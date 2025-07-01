c781b086e86b0f949f4eab2696aaf241f76ba196
King123XZ <yersonorosco2002@gmail.com>
2025-06-30 20:00:19 -0500

    Update mi.id.js

--- plugins/mi.id.js
+++ plugins/mi.id.js
@@ -1,1 +1,11 @@
+let handler = async (m, { conn }) => {
+  const miJid = conn.user.id   // JID de tu bot (tú)
+  const numero = miJid.split('@')[0] // solo número
+
+  await m.reply(`✅ *Mi JID:* ${miJid}\n📌 *Mi número:* ${numero}`)
+}
+
+handler.command = /^mijid$/i  // Escribes: !mijid
+
+export default handler
 
